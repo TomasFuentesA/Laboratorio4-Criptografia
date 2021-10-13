ad9715d7aa61a8a5f527a47ab9bbedb922902eba
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2021-10-12 21:10:45 -0330

    Hito II

--- main.cpp
+++ main.cpp
@@ -4,18 +4,49 @@
 #include <fstream>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+#include <ctime>
 using namespace std;
 
 
+/*************************
+La funcion tomi_hash toma el string
+y gracias a una seed que cambia cada hora, genera numeros aleatorios
+entre 33 y 126 (codigo ascii)
+Se verifica cual es mayor y cual es menor, entre
+El codigo ASCII del caracter y nuestro numero aleatorio
+se realiza una pequeña operacion matematica y se verifica que el valor
+no se salga del rango 33-126, en caso de escaparse, se genera un nuevo numero aleatorio
+Una vez obtenido el digito nuevo, se procede a calcular su caracter asociado y ser concatenado
+Al string "Hasheado"
+**************************/
+
 void tomi_hash(vector<char> caracteres) {
-    srand(2004);
+    time_t ttime = time(0);
+    tm *local_time = localtime(&ttime);
+    srand(local_time->tm_hour*60);
     std::vector<char> palabra_hasheada;
     for (int i = 0; i < 25 ; i++) {
-        int num = rand() % 126 + 32;
-        if (int(caracteres[i]) > num) {       
-            palabra_hasheada.push_back(char(int(caracteres[i]) - num +32 ));           
+        int num = 33 + rand() % (125-33);
+        if (int(caracteres[i]) > num) {
+            if ( (int(caracteres[i]) - num +32 < 33) ||  (int(caracteres[i]) - num +32 > 126) ) {
+                
+                int num_2 = 33 + rand() % (125-33);
+                palabra_hasheada.push_back(char(num_2));    
+            } else {
+                palabra_hasheada.push_back(char(int(caracteres[i]) - num +32 ));   
+            }
+                  
+                    
         } else {
-            palabra_hasheada.push_back(char(num - int(caracteres[i])+32));         
+            
+            if ( (num - int(caracteres[i])+32 < 33) ||  (num - int(caracteres[i])+32 > 126) ) {
+                
+                int num_2 = 33 + rand() % (125 - 32);
+                palabra_hasheada.push_back(char(num_2));    
+            } else {
+                palabra_hasheada.push_back(char(num - int(caracteres[i])+32));   
+            }
+                    
         }   
     }
 
@@ -28,29 +59,59 @@ void tomi_hash(vector<char> caracteres) {
 
 }
 
+/*************************
+En caso de ser un archivo
+Se lee linea a linea el archivo
+Si el largo de la linea es menor a 25 
+se repite hasta que sea de ese largo.
+En caso de ser mayor se recorta el string
+De derecha a izquierda.
+**************************/
+
 void es_archivo(string nombre, string extension){
     string archivo = nombre + "." +extension;
     string linea;
-    cout << archivo << "\n";
     ifstream f(archivo.c_str());
     while (getline(f, linea)) {
         std::vector<char> cantidad;
-        for (int i = 0; i < linea.length() ; i++) {
+        for (long unsigned int i = 0; i < linea.length() ; i++) {
             cantidad.push_back(linea[i]);
+        }
+
+        if (cantidad.size() < 25) {
+        for (long unsigned int i = 0; i < (25 - linea.size()); i ++) {
+            cantidad.push_back(linea[i%linea.length()]);
+        }
+
+        } else {
+            while (cantidad.size() > 25) {
+                cantidad.pop_back();
+            }
         }
         // Lo vamos imprimiendo
         tomi_hash(cantidad);
     }
 }
 
+/*************************
+En caso de no ser un archivo
+Se verifica el largo del string
+Si es menor a 25 se repite hasta que
+sea de ese largo.
+En caso de ser mayor se recorta el string
+De derecha a izquierda
+**************************/
+
 void no_archivo(string texto) {
+    
     std::vector<char> cantidad;
-    for (int i = 0; i < texto.length() ; i++) {
+    for (long unsigned int i = 0; i < texto.length() ; i++) {
+        
         cantidad.push_back(texto[i]);
     }
 
     if (cantidad.size() < 25) {
-        for (int i = 0; i < (25 - texto.size()); i ++) {
+        for (long unsigned int i = 0; i < (25 - texto.size()); i ++) {
             cantidad.push_back(texto[i%texto.length()]);
         }
 
@@ -59,7 +120,6 @@ void no_archivo(string texto) {
             cantidad.pop_back();
         }
     }
-    cout << cantidad.size() << "\n";
     tomi_hash(cantidad);
     
     
@@ -68,9 +128,12 @@ void no_archivo(string texto) {
 
 int main (int argc, char *argv[])
 {
+    
+    //En el main se detecta si es un archivo o un string simple
     if(argc!=2) {
-        es_archivo(argv[1], argv[2]);
+        es_archivo(argv[1], argv[2]); 
     } else if (argc == 2) {
+        
         no_archivo(argv[1]);
     } else {
         cout << "ERROR" << "\n";
